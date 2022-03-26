5577f676ffe7542da587b0c06ed700b006c7125e
chxyt <chxyt@live.cn>
2022-03-26 23:24:56 +0800

    this is a copy from kvm-guest-drivers-windows(https://github.com/virtio-win/kvm-guest-drivers-windows) commit: 3af3dd0f109e1a50ba2a242fab7c8d55b5ccd046

---
+++ NetKVM/CoInstaller/clean.bat
@@ -0,0 +1,25 @@
+@echo off
+goto start
+:rmdir
+if exist "%~1" rmdir "%~1" /s /q
+goto :eof
+
+:rmfiles
+if "%~1"=="" goto :eof
+if exist "%~1" del "%~1"
+shift
+goto rmfiles
+
+:start
+
+call :rmdir x64
+call :rmdir VistaDebug
+call :rmdir VistaRelease
+call :rmdir Win7Debug
+call :rmdir Win7Release
+call :rmdir Win8Debug
+call :rmdir Win8Release
+call :rmdir Win8.1Debug
+call :rmdir Win8.1Release
+call :rmdir Win10Release
+call :rmdir Win10Debug
